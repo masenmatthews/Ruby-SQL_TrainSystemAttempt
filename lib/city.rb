0c68299fa29f2586b85c608a763e71a916443a60
Masen Matthews <masenmatthews@gmail.com>
2018-02-21 10:12:30 -0800

    expand lib/back end

--- lib/city.rb
+++ lib/city.rb
@@ -1,12 +1,27 @@
 class City
-  @@city_list = []
+
+  attr_reader(:city_id, :stop_id, :arrival_time)
 
   def initialize(attributes)
     @city_id = attributes.fetch(:city_id)
     @stop_id = attributes.fetch(:stop_id)
-    @time = attributes.fetch(:time)
+    @arrival_time = attributes.fetch(:arrival_time)
   end
 
+  def ==(another_city)
+    self.city_id().==(another_city.city_id()).&(self.stop_id().==(another_city.stop_id()).&(self.arrival_time().==(another_city.arrival_time())
+  end
 
+  def city_id
+    @city_id
+  end
+
+  def stop_id
+    @stop_id
+  end
+
+  def arrival_time
+    @arrival_time
+  end
 
 end
--- lib/train.rb
+++ lib/train.rb
@@ -1,5 +1,6 @@
 class Train
-  @@train_list = []
+
+  attr_accessor(:train, :train_id, :train_route)
 
   def initialize(attributes)
     @train = attributes.fetch(:train)
@@ -7,6 +8,20 @@ class Train
     @train_route = attributes.fetch(:train_route)
   end
 
+  def ==(another_train)
+    self.train().==(another_train.train()).&(self.train_id().==(another_train.train_id()).&(self.train_route().==(another_train.train_route())
+  end
 
+  def train
+    @train
+  end
+
+  def train_id
+    @train_id
+  end
+
+  def train_route
+    @train_route
+  end
 
 end
